6784e212f046a1f07b1e595644614deaeb474ef8
Aakash <ashaakash219@gmail.com>
2022-03-27 21:39:35 +0530

    initial committing

--- week 3/DayPicker/app/src/main/java/com/example/daypicker/MainActivity.java
+++ week 3/DayPicker/app/src/main/java/com/example/daypicker/MainActivity.java
@@ -1,44 +1,55 @@
 package com.example.daypicker;
 
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Calendar;
+
 public class MainActivity extends AppCompatActivity {
-    DatePicker picker;
-    Button displayDate;
-    TextView textview1;
+    DatePickerDialog picker;
+    EditText eText;
+    Button btnGet;
+    TextView tvw;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        textview1=(TextView)findViewById(R.id.textView1);
-        picker=(DatePicker)findViewById(R.id.datePicker);
-        displayDate=(Button)findViewById(R.id.button1);
-
-        textview1.setText("Current Date: "+getCurrentDate());
-
-        displayDate.setOnClickListener(new View.OnClickListener(){
+        tvw=(TextView)findViewById(R.id.textView1);
+        eText=(EditText) findViewById(R.id.editText1);
+        eText.setInputType(InputType.TYPE_NULL);
+        eText.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-
-                textview1.setText("Change Date: "+getCurrentDate());
+            public void onClick(View v) {
+                final Calendar cldr = Calendar.getInstance();
+                int day = cldr.get(Calendar.DAY_OF_MONTH);
+                int month = cldr.get(Calendar.MONTH);
+                int year = cldr.get(Calendar.YEAR);
+                // date picker dialog
+                picker = new DatePickerDialog(MainActivity.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                                eText.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+                            }
+                        }, year, month, day);
+                picker.show();
             }
-
         });
-
-    }
-    public String getCurrentDate(){
-        StringBuilder builder=new StringBuilder();;
-        builder.append((picker.getMonth() + 1)+"/");//month is 0 based
-        builder.append(picker.getDayOfMonth()+"/");
-        builder.append(picker.getYear());
-        return builder.toString();
+        btnGet=(Button)findViewById(R.id.button1);
+        btnGet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvw.setText("Selected Date: "+ eText.getText());
+            }
+        });
     }
 }
